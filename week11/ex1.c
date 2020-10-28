a5038544cac40b6d900d2479dac8288d54f9c2c1
Danis Alukaev <d.alukaev@innopolis.university>
2020-10-28 09:32:53 +0300

    week11

--- week11/ex1.c
+++ week11/ex1.c
@@ -9,25 +9,78 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
+#define MAX_LENGTH 15
+
 char* filepath = "ex1.txt";
 char* my_string = "This is a nice day";
 
+/**
+ * Opens a file with the specified path and writes a randomly generated string in it.
+ *
+ * @param filepath - path to the file.
+ */
+void generate_file(char* filepath)
+{
+    // Open the file.
+    FILE *new_file = fopen(filepath, "w");
+    // Generate seed.
+    srand(time(0));
+    // Define the length of the random string.
+    int string_size = rand() % MAX_LENGTH + MAX_LENGTH / 4;
+    // Allocate memory for the string.
+    char* string = malloc(string_size * sizeof(char));
+    // Define the set of letters that the word will consist of.
+    int charset_size = 70;
+    char* charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?! ";
+    // Append random letters to string.
+	for (int i = 0; i < string_size-1; i++)
+        string[i] = charset[rand() % charset_size];
+    // Denote the end of the string.
+    string[string_size-1] = '\0';
+    // Write the string in file.
+	fprintf(new_file, string);
+    // Close the file.
+	fclose(new_file);
+}
+
 int main()
 {
+    // Generate the string and write it in file.
+    generate_file(filepath);
     // Open the file in O_RDWR mode.
     // System call open returns the file descriptor.
     int descriptor = open(filepath, O_RDWR);
+    // Handle an opening failure.
+    if(descriptor == -1)
+    {
+        printf("Fail to open file.\n");
+        return -1;
+    }
     // Determine the string length.
     int string_len = strlen(my_string);
     // System call fstat utilizes structures stat.
     // Create an instance of this structure.
     struct stat info;
     // Retrieve information about the file using its descriptor.
-    fstat(descriptor, &info);
+    int fstat_r = fstat(descriptor, &info);
+    // Check fstat returned error.
+    if(fstat_r == -1)
+    {
+        printf("Error occurred while fstat() using.\n");
+        close(descriptor);
+        return -1;
+    }
     // Determine the size of file, in bytes.
     int file_size = info.st_size;
     // Create new mapping in the virtual address space.
     char* area = mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, descriptor, 0);
+    // Handle a mapping failure.
+    if(area == -1)
+    {
+        printf("Fail to create new mapping.");
+        close(descriptor);
+        return -1;
+    }
     // Truncate the file to string length.
     ftruncate(descriptor, string_len);
     // Copy new string to the mapped area.
--- week11/ex4.c
+++ week11/ex4.c
@@ -14,15 +14,30 @@ char* target = "ex1.memcpy.txt";
 
 int main()
 {
-    // Open the files in O_RDWR mode.
-    // System call open returns the file descriptor.
-    int d_source = open(source, O_RDWR);
-    int d_target = open(target, O_RDWR);
+    // Open the source file in O_RDONLY mode.
+    // Create target file if it does not exist and open it in O_RDWR mode.
+    // System call open returns the file descriptor, save it for both files.
+    int d_source = open(source, O_RDONLY);
+    int d_target = open(target, O_CREAT | O_RDWR, 0777);
+    // Handle an opening failure.
+    if(d_source == -1 || d_target == -1)
+    {
+        printf("Fail to open files.\n");
+        return -1;
+    }
     // System call fstat utilizes structures stat.
     // Create an instance of this structure.
     struct stat info;
     // Retrieve information about the source file using its descriptor.
-    fstat(d_source, &info);
+    int fstat_r = fstat(d_source, &info);
+    // Check fstat returned error.
+    if(fstat_r == -1)
+    {
+        printf("Error occurred while fstat() using.\n");
+        close(d_source);
+        close(d_target);
+        return -1;
+    }
     // Determine the size of file, in bytes.
     int source_size = info.st_size;
     // Truncate the target file to size of source file.
@@ -30,12 +45,20 @@ int main()
     // Create new mappings in the virtual address space.
     char* a_source = mmap(NULL, source_size, PROT_READ, MAP_SHARED, d_source, 0);
     char* a_target = mmap(NULL, source_size, PROT_WRITE, MAP_SHARED, d_target, 0);
+    // Handle a mapping failure.
+    if(a_source == -1 || a_target == -1)
+    {
+        printf("Fail to create new mapping.");
+        close(d_source);
+        close(d_target);
+        return -1;
+    }
     // Copy new string to the mapped area.
     strncpy(a_target, a_source, source_size);
     // Delete the mapping.
     munmap(a_source, source_size);
     munmap(a_target, source_size);
-    close(a_source);
-    close(a_target);
+    close(d_source);
+    close(d_target);
     return 0;
 }
